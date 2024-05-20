558e12244d64124929cf0618cfeb598148ceb649
Arnab Chakraborty <chakra56@msu.edu>
2024-05-19 21:50:35 -0700

    sum of two numbers.

---
+++ sumNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    // get or define the numbers
+    int firstNumber = 12;
+    int secondNumber = 9;
+
+    // sum the numbers
+    int sum = firstNumber + secondNumber;
+    
+    // print the sum
+    std::cout << "The sum of the two numbers is: " << sum << std::endl;
+    
+    // terminate the program
+    return 0;
+}
